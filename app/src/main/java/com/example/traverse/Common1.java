140f13082d87b6af3395d60480b9f4641c056fe5
Shivam <shuklashivamk2@gmail.com>
2019-05-08 01:31:34 +0530

    Third commit

---
+++ app/src/main/java/com/example/traverse/Common1.java
@@ -0,0 +1,22 @@
+package com.example.traverse;
+
+public class Common1 {
+
+    private String name;
+    private String description;
+
+    public Common1(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
+
+
---
+++ app/src/main/java/com/example/traverse/Common1Adapter.java
@@ -0,0 +1,41 @@
+package com.example.traverse;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Common1Adapter extends ArrayAdapter<Common1> {
+
+    private static final String LOG_TAG = Common1Adapter.class.getSimpleName();
+
+    public Common1Adapter(Activity context, ArrayList<Common1> list) {
+        super(context, 0, list);
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+        Common1 currentplace = getItem(position);
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.text_view1);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        nameTextView.setText(currentplace.getName());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView numberTextView = (TextView) listItemView.findViewById(R.id.text_view2);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        numberTextView.setText(currentplace.getDescription());
+
+        return listItemView;
+    }
+}
