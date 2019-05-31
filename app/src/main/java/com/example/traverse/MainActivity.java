6fc7b7278b9d231f494bd570a750a25f3370e4f9
Shivam <shuklashivamk2@gmail.com>
2019-05-31 16:17:25 +0530

    Added toast for Haridwar and Madras activity

--- app/src/main/java/com/example/traverse/MainActivity.java
+++ app/src/main/java/com/example/traverse/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.traverse;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +30,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent= new Intent(MainActivity.this,Mussoorie.class);
                 startActivity(intent);
+            }
+        });
+        CardView cardView2 =(CardView)findViewById(R.id.haridwar);
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Work in progress";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        CardView cardView3 =(CardView)findViewById(R.id.madras);
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Work in progress";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
     }
